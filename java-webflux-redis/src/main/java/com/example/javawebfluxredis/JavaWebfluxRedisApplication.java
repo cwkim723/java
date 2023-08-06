a654026e8d4c1825d45b57e0f05df43286f97dd4
Chaewon Kim <irene4563@naver.com>
2023-08-07 00:17:14 +0900

    init

---
+++ java-webflux-redis/src/main/java/com/example/javawebfluxredis/JavaWebfluxRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawebfluxredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebfluxRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebfluxRedisApplication.class, args);
+	}
+
+}
---
+++ java-webflux-redis/src/test/java/com/example/javawebfluxredis/JavaWebfluxRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebfluxredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebfluxRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
